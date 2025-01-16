5afa578852721d21ea22ae0f5e8aecc980f7c908
Muhammaddavron <muhammaddavron123@gmail.com>
2025-01-16 12:22:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const typed = new Typed('#text', {
+  strings: ['<i>First</i> sentence.', 'Text Gradients'],
+  startDelay: 1000,
+  typeSpeed: 100,
+  backSpeed:100,
+  backDelay: 70,
+    loop:true,
+});
+
+const typed2 = new Typed('#text2', {
+    strings: [ 'Make your headlines stand out'],
+    startDelay: 500,
+    typeSpeed: 70,
+    backSpeed:70,
+    backDelay: 700,
+      loop:true,
+
+  });
+  
